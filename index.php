7a3e60f5960fadda69610eb17a076f32e036625c
Andrii Pereverziev <a.pereverziev@enamine.net>
2022-09-05 14:13:44 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,127 @@
+
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="TonePHP Framework">
+    <meta name="author" content="Andrii Pereverziev">
+    <meta name="generator" content="Hugo 0.101.0">
+    <title>TonePHP Framework</title>
+
+<link href="./bootstrap.min.css" rel="stylesheet">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="./cover.css" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">TonePHP Main Page</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="/">Home</a>
+      </nav>
+    </div>
+  </header>
+
+  <main class="px-3">
+    <h1>Eight Green Jumpers in progress!</h1>
+    <p class="lead">
+      <a href="https://eight-green-jumpers.tonephp.com" class="btn btn-lg btn-secondary fw-bold border-white bg-white">See the page</a>
+    </p>
+
+    <h2>All frameworks</h2>
+    <table class="table table-dark table-striped">
+        <tr>
+            <td>Red Socks</td>
+            <td>
+                <a href="https://red-socks.tonephp.com">https://red-socks.tonephp.com</a>
+            </td>
+            <td>
+                <a href="https://github.com/tonephp/red-socks-website">https://github.com/tonephp/red-socks-website</a>
+            </td>
+            <td>
+                <a href="https://github.com/tonephp/red-socks">https://github.com/tonephp/red-socks</a>
+            </td>
+        </tr>
+        <tr>
+            <td>Eight Green Jumpers</td>
+            <td>
+                <a href="https://eight-green-jumpers.tonephp.com/">https://eight-green-jumpers.tonephp.com/</a>
+            </td>
+            <td>
+                <a href="https://github.com/tonephp/eight-green-jumpers-website">https://github.com/tonephp/eight-green-jumpers-website</a>
+            </td>
+            <td>
+                <a href="https://github.com/tonephp/eight-green-jumpers">https://github.com/tonephp/eight-green-jumpers</a>
+            </td>
+        </tr>
+    </table>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>Made with interest by Andrii Pereverziev (webuxmotion)</p>
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
