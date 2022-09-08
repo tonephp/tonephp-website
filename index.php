dd975e4ed352f81f5484ef0b9967e8c8a8ad5e69
Andrii Pereverziev <a.pereverziev@enamine.net>
2022-09-08 14:12:56 -0700

    Logo

--- index.php
+++ index.php
@@ -74,10 +74,8 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">TonePHP Main Page</h3>
-      <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="/">Home</a>
-      </nav>
+      <img src="/logo.svg" alt="logo tonephp" style="width: 200px;">
+      
     </div>
   </header>
 
